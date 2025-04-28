e2dbcdd38304126def29b25fe8ef9942909b0d2a
Aminu Abubakar <amish2g@gmail.com>
2025-04-28 23:21:15 +0000

    copy front-end dist build folder to backend

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -95,7 +96,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+//const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
