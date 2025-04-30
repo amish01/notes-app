152ce35c7a99b587ccf15cb4ecb231c62231ecbb
Aminu Abubakar <amish2g@gmail.com>
2025-04-30 14:53:16 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const cors = require('cors')
+
 
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
 
 let notes = [
@@ -26,16 +25,6 @@ let notes = [
     }
   ]
 
-  // //middleware for logging info about requests
-  // const requestLogger = (request, response, next) => {
-  //   console.log('Method:', request.method)
-  //   console.log('Path:  ', request.path)
-  //   console.log('Body:  ', request.body)
-  //   console.log('---')
-  //   next()
-  // }
-
-  //app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -96,7 +85,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-//const PORT = 3001
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
