3931c6948e2053aa953a6a17c310ce8d12fb6875
Aminu Abubakar <amish2g@gmail.com>
2025-08-13 19:24:32 +0000

    Move mongoDB config to its own module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
+
+
 
 app.use(morgan('tiny'))
 app.use(express.json())
@@ -10,26 +12,11 @@ app.use(express.static('dist'))
 
 
 // DO NOT SAVE YOUR PASSWORD TO GITHUB!!
-const password = process.argv[2]
-const url = `mongodb+srv://amish2g:${password}@cluster0.xtbuvch.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
 
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
 
-const Note = mongoose.model('Note', noteSchema)
+//const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
     {
