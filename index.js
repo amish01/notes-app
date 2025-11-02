978c05877efbaf5f6b322df7dc3ecffd931ce92b
Aminu Abubakar <amish2g@gmail.com>
2025-11-02 08:07:15 +0000

    Implement backend routing and error handling with database

--- index.js
+++ index.js
@@ -19,57 +19,51 @@ app.use(express.static('dist'))
 
 //const Note = mongoose.model('Note', noteSchema)
 
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+// let notes = [
+//     {
+//       id: "1",
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: "2",
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: "3",
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
 
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/notes', (request, response, next) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
+  .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    // const id = request.params.id
-    // const note = notes.find(note => note.id === id)
-    // if(note){
-    //     response.json(note)
-    // } else {
-    //     response.status(404).end()
-    // }
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+      if(note){
+        response.json(note)
+      }else {
+        response.status(404).end()
+      }    
     })
+    .catch(error => next(error))
+    
   
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => Number(n.id)))
-  : 0
-  return String(maxId + 1)
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
  
   if(!body.content) {
@@ -77,31 +71,41 @@ app.post('/api/notes', (request, response) => {
       error: 'content missing'
     })
   }
-
-  // const note = {
-  //   content: body.content,
-  //   important: body.important || false,
-  //   id: generateId(),
-  // }
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
-
-
-  // notes = notes.concat(note)
-  
-  // response.json(note)
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes.filter(note => note.id !== id)
-
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
     response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -109,6 +113,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
--- mongo.js
+++ mongo.js
@@ -21,50 +21,13 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//   content: 'CSS is tricky',
-//   important: true,
-// })
+const note = new Note({
+  content: 'CSS is tricky 2',
+  important: true,
+})
 
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
-
-
-
-
-// const mongoose = require('mongoose');
-
-
-// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-// if (process.argv.length < 3) {
-//   console.log('give password as argument')
-//   process.exit(1)
-// }
-
-// const password = process.argv[2]
-
-// const uri = `mongodb+srv://amish2g:${password}@cluster0.xtbuvch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// async function run() {
-//   try {
-//     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-//     await mongoose.connect(uri, clientOptions);
-//     await mongoose.connection.db.admin().command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await mongoose.disconnect();
-//   }
-// }
-// run().catch(console.dir);
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
